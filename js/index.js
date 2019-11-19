236533c032ee0f8ad7fd10f8dec074c864830cdb
designervoid <newluciocheg@gmail.com>
2019-11-19 18:34:57 +0300

    to prev step function

--- js/index.js
+++ js/index.js
@@ -16,5 +16,28 @@ let toNextStep = function (previousStepButtonId, previousStepBlockId, nextStepBl
     this.nextStepBlockDisplayBlock();
 }
 
-// toNextStep('next-button-step-first', 'modal-content-step-first', 'modal-content-step-second');
-// toNextStep('next-button-step-second', 'modal-content-step-second', 'modal-content-step-third');
+let toPreviousStep = function (currentStepButtonId, currentStepBlockId, previousStepBlockId) {
+    this.currentStepButtonId = currentStepButtonId;
+    this.currentStepBlockId = currentStepBlockId;
+    this.previousStepBlockId = previousStepBlockId;
+
+    this.currentStepBlockDisplayNone = function () {
+        this.currentStepBlock = document.getElementById(currentStepBlockId);
+        this.currentStepBlock.style['display'] = 'none';
+    }
+    this.currentStepBlockDisplayNone();
+
+    this.previousStepBlockDisplayBlock = function () {
+        this.previousStepBlock = document.getElementById(previousStepBlockId);
+        this.previousStepBlock.style['display'] = 'block';
+    }
+    this.previousStepBlockDisplayBlock();
+}
+
+// toNextStep('next-button-step-first', 'modal-content-step-first', 'modal-content-step-second'); -> to second step
+
+// toNextStep('next-button-step-second', 'modal-content-step-second', 'modal-content-step-third'); -> to third step
+
+// toPreviousStep('previous-button-step-third', 'modal-content-step-third', 'modal-content-step-second'); -> back to second step
+
+// toPreviousStep('previous-button-step-second', 'modal-content-step-second', 'modal-content-step-first'); -> back to first step 
